8635e9f4287d6d354f163156b4ab5c71c605b03f
Ankit Kumar Gautam <ankitgau@andrew.cmu.edu>
2020-10-26 14:23:57 -0400

    r testing

--- r-dir-script.sh
+++ r-dir-script.sh
@@ -5,16 +5,20 @@
 cd project
 cd src
 
-count = `ls -1 *.py 2>/dev/null | wc -l`
-count1 = `ls -1 *.r 2>/dev/null | wc -l`
+py_files = (' find ./ -maxdepth 1 -name "*.py" ')
+
+r_files =('find ./ -maxdepth 1 -name "*.r" ')
 
 cd ..
 
-if [ $count != 0 ]; then 
-echo "Python project"
-python -m pytest
-elif [ $count1 !=0 ]; then
+if [ ${#py_files[@]} -gt 0 ]; then 
+	echo "Python project"
+	python -m pytest
+
+elif [ ${#r_files[@]} -gt 0 ]; then
 	sudo apt install r-base
+
+	# https://stackoverflow.com/a/20924082
 	apt-get -y build-dep libcurl4-gnutls-dev
 	apt-get -y install libcurl4-gnutls-dev
 	Rscript -e 'install.packages(c("devtools","testthat"))'
