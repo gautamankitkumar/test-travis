a6141d48d9745af5bf246b671f3785edc800970b
Ankit Kumar Gautam <ankitgau@andrew.cmu.edu>
2020-11-02 12:01:31 -0500

    remove description

--- r-dir-script.sh
+++ r-dir-script.sh
@@ -7,16 +7,13 @@ cd src
 
 count=`ls -1 *.py 2>/dev/null | wc -l`
 
-count2=`ls -1 *.r 2>/dev/null | wc -l`
-
 
 cd ..
 
 if [ "$count" != 0 ]; then 
 	echo "Python project"
 
-elif [ "$count2" != 0 ]; then
-
+else
 	# https://stackoverflow.com/a/20924082
 	Rscript -e "install.packages(c('devtools','testthat'))"
 	Rscript -e 'library(testthat); test_dir("test/")'
