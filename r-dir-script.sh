106aa3efa254d2062bc50b672e769585ba702fb3
Ankit Kumar Gautam <ankitgau@andrew.cmu.edu>
2020-11-02 12:08:21 -0500

    Upadte checking code in script

--- r-dir-script.sh
+++ r-dir-script.sh
@@ -11,10 +11,11 @@ count=`ls -1 *.py 2>/dev/null | wc -l`
 cd ..
 
 if [ "$count" != 0 ]; then 
-	echo "Python project"
+	python -m pytest
 
 else
+
 	# https://stackoverflow.com/a/20924082
-	Rscript -e "install.packages(c('devtools','testthat'))"
+	# Rscript -e "install.packages(c('devtools','testthat'))"
 	Rscript -e 'library(testthat); test_dir("test/")'
 fi
